0d8bb1f78dbe084f900151ab8116591e5ff1f516
ZhangTony112 <a18092294585@163.com>
2023-06-02 00:55:56 +0800

    feat(hooks): 修复验证

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 11111222221112222222222
+          Learn React 111
         </a>
       </header>
     </div>
