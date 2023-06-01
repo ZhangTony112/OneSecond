89541bdee512757f0e104c02834588caba356fde
ZhangTony112 <a18092294585@163.com>
2023-06-01 23:18:13 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 111112222223333
+          Learn React 11111222222
         </a>
       </header>
     </div>
